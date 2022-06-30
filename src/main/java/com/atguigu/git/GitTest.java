b1bde53e62b62b08bee793be7e37f3a34c62affa
shangyongbin <46081682+shangyongbin@users.noreply.github.com>
2022-07-01 00:38:58 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git...");
         System.out.println("333333333333");
+        System.out.println("github online commit");
     }
 }
