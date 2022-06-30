48953233034143f8c336e9166ea03dcfb6b44081
shangyongbin <ybshang@foxmail.com>
2022-07-01 01:07:23 +0800

    gitee

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git...");
         System.out.println("333333333333");
         System.out.println("github online commit");
+        System.out.println("gitee test");
     }
 }
